27c488d8998767738e9dff8ff5c12998640c63f0
dianadiv <diana.gs202@gmail.com>
2023-10-10 17:56:26 +0300

    fix js

--- index.js
+++ index.js
@@ -39,74 +39,69 @@ const PLANS = [
 
 const container = document.getElementById("plans-container");
 
-PLANS.forEach(plan => {
-    const planContainer = document.createElement("div");
-    planContainer.classList.add("plan");
-
-    const circle = document.createElement("div");
-    circle.classList.add('plan__icon');
-    planContainer.appendChild(circle);
-
-    const planName = document.createElement("h2");
-    planName.textContent = plan['plan-name'];
-    planName.classList.add('plan__title');
-    planContainer.appendChild(planName);
-
-    const planPriceBlock = document.createElement("div");
-
-    const planPrice = document.createElement("span");
-    planPrice.textContent = plan['annual-price'] + ' $';
-    planPrice.classList.add('plan__price__amount');
-    const planPriceByPeriod = document.createElement("span");
-    planPriceByPeriod.textContent = ' per year';
-    planPriceByPeriod.classList.add('plan__price__period');
-    planPriceBlock.appendChild(planPrice);
-    planPriceBlock.appendChild(planPriceByPeriod);
-
-    planPriceBlock.classList.add('plan__price')
-    planContainer.appendChild(planPriceBlock);
-
-    const featuresList = document.createElement("ul");
-    featuresList.classList.add('plan__list')
-
-    for (var feature in plan.features) {
-        const featureItem = document.createElement("li");
-        featureItem.classList.add('plan__item')
-        featureItem.textContent = ((plan.features[feature] ? '✔' : '✘' ) + feature);
-        featuresList.appendChild(featureItem);
-    }
-
-    planContainer.appendChild(featuresList);
-
-    const signUpButton = document.createElement("button");
-    signUpButton.classList.add('plan__button')
-    signUpButton.textContent = "Sign Up";
-    planContainer.appendChild(signUpButton);
-
-    container.appendChild(planContainer);
-});
-
 const btnContainer = document.querySelector(".btn-container");
 
-btnContainer.addEventListener("click", function () {
-    const yearPeriod= document.getElementById("period_mode").checked;
-    console.log(1)
-    updatePrices(yearPeriod);
-});
+function updatePrices() {
+    container.innerHTML = '';
 
-function updatePrices(yearPeriod) {
-    PLANS.forEach(function(plan, index) {
-        const planCard = document.querySelectorAll(".plan")[index];
-
-        const priceElement = planCard.querySelector(".plan__price__amount");
-        const periodElement = planCard.querySelector(".plan__price__period");
+    PLANS.forEach(plan => {
+        const yearPeriod= document.getElementById("period_mode").checked;
+        
+        const planContainer = document.createElement("div");
+        planContainer.classList.add("plan");
+    
+        const circle = document.createElement("div");
+        circle.classList.add('plan__icon');
+        planContainer.appendChild(circle);
+    
+        const planName = document.createElement("h2");
+        planName.textContent = plan['plan-name'];
+        planName.classList.add('plan__title');
+        planContainer.appendChild(planName);
+    
+        const planPrice = document.createElement("div");
+        planPrice.classList.add('plan__price');
+        const planPriceAmount = document.createElement("span");
+        planPriceAmount.classList.add('plan__price__amount');
+        const planPricePeriod = document.createElement("span");
+        planPricePeriod.classList.add('plan__price__period');
 
         if (yearPeriod) {
-            priceElement.textContent = plan['monthly-price'] + ' $';
-            periodElement.textContent = ' per month';
+            planPriceAmount.textContent = plan['monthly-price'] + ' $'; 
+            planPricePeriod.textContent = ' by month';
         } else {
-            priceElement.textContent = plan['annual-price'] + ' $';
-            periodElement.textContent = ' per year';
+            planPriceAmount.textContent = plan['annual-price'] + ' $'; 
+            planPricePeriod.textContent = ' by year';
         }
+
+        planPrice.appendChild(planPriceAmount);
+        planPrice.appendChild(planPricePeriod);
+    
+        planContainer.appendChild(planPrice);
+    
+        const featuresList = document.createElement("ul");
+        featuresList.classList.add('plan__list')
+    
+        for (var feature in plan.features) {
+            const featureItem = document.createElement("li");
+            featureItem.classList.add('plan__item')
+            featureItem.textContent = ((plan.features[feature] ? '✔' : '✘' ) + feature);
+            featuresList.appendChild(featureItem);
+        }
+    
+        planContainer.appendChild(featuresList);
+    
+        const signUpButton = document.createElement("button");
+        signUpButton.classList.add('plan__button')
+        signUpButton.textContent = "Sign Up";
+        planContainer.appendChild(signUpButton);
+    
+        container.appendChild(planContainer);
     });
 }
+
+btnContainer.addEventListener("click", function () {
+    updatePrices();
+});
+
+updatePrices();
